00a6304079e4306021caa6ee014ec643cb9cd886
Zakir Khan <zakir.ghouri.dev@gmail.com>
2026-08-01 14:20:18 +0530

    Update Cross-entropy loss (Math Foundations 4/6)

--- foundations/loss.py
+++ foundations/loss.py
@@ -9,15 +9,16 @@ class Solution:
         # y_pred: predicted probabilities
         # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
         # return round(your_answer, 4)
-        n = y_true.size
-        result = np.where(y_true == 1, np.log(1e-7 + y_pred), np.log(1e-7 + 1 - y_pred))
-        return np.abs(np.round(np.sum(result) / n, 4))
+        
+        loss_element = np.where(y_true == 1, np.log(y_pred + 1e-7), np.log(1 - y_pred + 1e-7))
+        summation = np.sum(loss_element) / y_true.shape[0]
+        return np.round(-summation, 4)
 
     def categorical_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
         # y_true: one-hot encoded true labels (shape: n_samples x n_classes)
         # y_pred: predicted probabilities (shape: n_samples x n_classes)
         # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
         # return round(your_answer, 4)
-        n = y_true.shape[0]
-        result = np.where(y_true == 1, np.log(1e-7 + y_pred), 0)
-        return np.abs(np.round(np.sum(result) / n, 4))
+        loss_element = np.where(y_true == 1, np.log(y_pred + 1e-7), 0)
+        summation = np.sum(loss_element) / y_true.shape[0]
+        return np.round(-summation, 4)
