0aebde908c55089f5eef0ef30d6560de8a4288d6
Zakir Khan <zakir.ghouri.dev@gmail.com>
2026-08-05 11:54:19 +0530

    Update Training loop (Training 1/4)

--- foundations/training_loop.py
+++ foundations/training_loop.py
@@ -15,21 +15,21 @@ class Solution:
         # Initialize w = zeros, b = 0
         # return (np.round(w, 5), round(b, 5))
         y = np.atleast_2d(y).T #(batch_size, output_dim)
-
+        
         size = (y.shape[1], X.shape[1]) #(output_dim, input_dim)
         N = y.shape[0]
 
-        w = np.zeros(size) #(output_dim, input_dim)
-        b = np.zeros(y.shape[1]) #(output_dim)
-
+        w = np.zeros(size)
+        b = np.zeros(y.shape[1])
         for i in range(epochs):
             y_hat = X @ w.T + b #(batch_size, output_dim)
 
-            #gradients
             dl_dy = 2 * (y_hat - y) / N #(batch_size, output_dim)
             dl_dw = dl_dy.T @ X
+
             dl_db = np.sum(dl_dy, axis=0)
 
             w -= lr * (dl_dw)
-            b -= lr * (dl_db)
+            b -= lr *(dl_db)
+
         return (np.round(w.reshape(-1,), decimals=5), round(b.item(), 5))
